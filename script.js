d75f5d5b6cf2d6634fba8d5fa8aeebc81e8ba57d
dheeraj1566 <dheerajjangid013@gmail.com>
2025-05-20 13:12:21 +0530

    design name text 4

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-  const menuToggle = document.getElementById('menuToggle');
-  const navLinks = document.getElementById('navLinks');
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
 
-  menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-  });
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  if (window.scrollY > window.innerHeight) {
+    header.classList.add("shrink");
+  } else {
+    header.classList.remove("shrink");
+  }
+});
