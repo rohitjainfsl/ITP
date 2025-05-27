f45b9221822ddbb30af7e4b43ef65afa3a9800fb
dheeraj1566 <dheerajjangid013@gmail.com>
2025-05-27 11:41:21 +0530

    update 1

--- script.js
+++ script.js
@@ -37,25 +37,26 @@ document.addEventListener("DOMContentLoaded", () => {
   closeIcon.style.display = "none";
 });
 
-$(".owl-carousel").ready(function () {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: true,
-      },
-      600: {
-        items: 3,
-        nav: false,
-      },
-      1000: {
-        items: 5,
-        nav: true,
-        loop: false,
-      },
-    },
+
+
+  $(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 2
+        },
+        1000: {
+          items: 3
+        }
+      }
+    });
   });
-});
